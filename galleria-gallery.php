fb8260d0c5ecdba079705f262dde556611e841b3
Pierre Rudloff <contact@rudloff.pro>
2015-12-04 17:40:01 +0100

    Enqueue scripts in hook

--- galleria-gallery.php
+++ galleria-gallery.php
@@ -7,9 +7,9 @@
  * Author URI: https://rudloff.pro/
  * License: GPL3
  * Plugin URI: https://github.com/Rudloff/wp-galleria-gallery
- * 
+ *
  * PHP version 5.6
- * 
+ *
  * @category WordPress
  * @package  GalleriaGallery
  * @author   Pierre Rudloff <contact@rudloff.pro>
@@ -17,19 +17,23 @@
  * @link     https://github.com/Rudloff/wp-galleria-gallery
  */
 
-wp_enqueue_script(
-    'galleria',
-    plugin_dir_url(__FILE__).'/bower_components/galleria/src/galleria.js',
-    array('jquery')
-);
-wp_enqueue_script(
-    'galleria-classic',
-    plugin_dir_url(__FILE__)
-    .'/bower_components/galleria/src/themes/classic/galleria.classic.js',
-    array('galleria')
-);
-wp_enqueue_script(
-    'galleria-gallery',
-    plugin_dir_url(__FILE__).'/galleria-gallery.js',
-    array('galleria-classic')
-);
+function galleria_scripts() {
+    wp_enqueue_script(
+        'galleria',
+        plugin_dir_url(__FILE__).'/bower_components/galleria/src/galleria.js',
+        array('jquery')
+    );
+    wp_enqueue_script(
+        'galleria-classic',
+        plugin_dir_url(__FILE__)
+        .'/bower_components/galleria/src/themes/classic/galleria.classic.js',
+        array('galleria')
+    );
+    wp_enqueue_script(
+        'galleria-gallery',
+        plugin_dir_url(__FILE__).'/galleria-gallery.js',
+        array('galleria-classic')
+    );
+}
+
+add_action('wp_enqueue_scripts', 'galleria_scripts');
